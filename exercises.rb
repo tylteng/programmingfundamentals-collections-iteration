91ab6a66e959e1d77b390566b2d998c047abbe61
tylteng <teng.ty4@gmail.com>
2017-11-01 16:19:14 -0400

    finished exercise 1

--- exercises.rb
+++ exercises.rb
@@ -1,8 +1,35 @@
 # exercise 0
 fav_colors = ['blue', 'pink', 'black', 'white']
 generation_x = [20, 24, 17, 19, 19, 19, 15, 6, 4]
-coin_flip = ['head', 'heads', 'heads', 'heads', 'heads']
+coin_flip = ['heads', 'heads', 'heads', 'heads', 'heads']
 fav_artists = ['Travis Scott', 'Kanye West', 'The Internet']
 fav_colors_symbols = [':blue', ':pink', ':black', ':white']
 
+definitions = { :bonce => "head; skull",
+  :heebie_jeebies => "a condition of extreme nervousness caused by fear",
+  :kobold => "a spirit or goblin" }
+
+movies = { Tokyo_Drift: 2006,
+  Kill_Bill: 2003,
+  Spirited_Away: 2001 }
+
+cities = { NYC: 8500000,
+  Japan: 9260000,
+  London: 8630000 }
+
+siblings = { Tyler: 20,
+  Travis: 19,
+  Terence: 17 }
+
+
+
 # exercise 1
+puts coin_flip #1
+puts "-"*30
+puts fav_colors[0] #2
+puts "-"*30
+puts generation_x.sort #3
+puts "-"*30
+generation_x << 0 #4
+puts generation_x
+puts "-"*30
