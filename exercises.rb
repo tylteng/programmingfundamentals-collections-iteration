626820b2272cb0e145df610a20ba0b7a4e600eee
tylteng <teng.ty4@gmail.com>
2017-11-01 15:45:44 -0400

    finished exercise 0

---
+++ exercises.rb
@@ -0,0 +1,8 @@
+# exercise 0
+fav_colors = ['blue', 'pink', 'black', 'white']
+generation_x = [20, 24, 17, 19, 19, 19, 15, 6, 4]
+coin_flip = ['head', 'heads', 'heads', 'heads', 'heads']
+fav_artists = ['Travis Scott', 'Kanye West', 'The Internet']
+fav_colors_symbols = [':blue', ':pink', ':black', ':white']
+
+# exercise 1
