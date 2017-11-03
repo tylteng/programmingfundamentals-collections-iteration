bad4a34487eb84b6a3723d9109219c9010284ae6
tylteng <teng.ty4@gmail.com>
2017-11-03 09:58:44 -0400

    finished exercise 2

--- exercises.rb
+++ exercises.rb
@@ -1,35 +1,61 @@
 # exercise 0
 fav_colors = ['blue', 'pink', 'black', 'white']
 generation_x = [20, 24, 17, 19, 19, 19, 15, 6, 4]
-coin_flip = ['heads', 'heads', 'heads', 'heads', 'heads']
+coin_flip = ['heads', 'heads', 'heads', 'heads', 'tails']
 fav_artists = ['Travis Scott', 'Kanye West', 'The Internet']
 fav_colors_symbols = [':blue', ':pink', ':black', ':white']
 
-definitions = { :bonce => "head; skull",
+definitions = {
+  :bonce => "head; skull",
   :heebie_jeebies => "a condition of extreme nervousness caused by fear",
   :kobold => "a spirit or goblin" }
 
-movies = { Tokyo_Drift: 2006,
+movies = {
+  Tokyo_Drift: 2006,
   Kill_Bill: 2003,
   Spirited_Away: 2001 }
 
-cities = { NYC: 8500000,
+cities = {
+  NYC: 8500000,
   Japan: 9260000,
   London: 8630000 }
 
-siblings = { Tyler: 20,
+siblings = {
+  Tyler: 20,
   Travis: 19,
   Terence: 17 }
-
-
 
 # exercise 1
 puts coin_flip #1
 puts "-"*30
+
 puts fav_colors[0] #2
 puts "-"*30
+
 puts generation_x.sort #3
 puts "-"*30
+
 generation_x << 0 #4
 puts generation_x
 puts "-"*30
+
+puts movies[:Tokyo_Drift]
+puts "-"*30
+# exercise 2
+puts fav_colors.last
+puts "-"*30
+
+cities[:Paris] = 10500000
+puts cities
+puts "-"*30
+
+coin_flip.reverse!
+puts coin_flip
+puts "-"*30
+
+puts cities[:Paris]
+puts "-"*30
+
+puts "I think #{fav_artists[0]} is great."
+puts "I think #{fav_artists[1]} is great."
+puts "I think #{fav_artists[2]} is great."
