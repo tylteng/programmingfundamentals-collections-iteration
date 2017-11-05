a484a911fcd16d2b665327b24d1615eaad49232a
tylteng <teng.ty4@gmail.com>
2017-11-04 22:36:58 -0400

    finished exercise 6

---
+++ exercise6.rb
@@ -0,0 +1,13 @@
+movies = {
+  1999: ["The Matrix", "Star Wars: Episode 1", "The Mummy"]
+  2009: ["Avatar", "Star Trek", "District 9"]
+  2019: ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+}
+
+phone_buttons = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+countries = [
+  {name: Manhattan, population: 8500000, island: yes},
+  {name: London, population: 8630000, island: yes},
+  {name: Tokyo, population: 9260000, island: yes}
+]
